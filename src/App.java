30a8c8c8d8b05ab5f13f70a8491c86a2c4b195c1
Lucas Augusto Hartmann <lucas.a111@hotmail.com>
2022-09-21 18:15:28 -0300

    Please enter the commit message for your changes. Lines starting Estou comitant na main

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ package src;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Lucasssssdsdsds");
+        System.out.println("Lucasssssdsdsds");
     }
 }
