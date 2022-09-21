eedfcf1f083318b926c31f0d7bc09e1a4f2e4cfc
Eduardo Riboli <EduardoRiboli71@gmail.com>
2022-09-21 18:31:43 -0300

    Luicas

--- App.java
+++ App.java
@@ -1,13 +1,10 @@
 public class App{
     public static void main(String[] args){
-        System.out.println("Ola mundo!");
+      
 
-        System.out.println("Cachorro");
-
-        System.out.println("Eu");
-
-        System.out.println("Tentando dnv");
-
-        System.out.println("Eu de novo");
+        System.out.println("Cachoor");
+        System.out.println("Cachoor");
+        System.out.println("Cachoor");
+        System.out.println("Cachoor");
     }
 }
