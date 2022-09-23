c12f0cb0cdd5e4d28fccff33ade13458e8dbc2ae
Matheus Fernandes da Rosa <matheusmemphis@gmail.com>
2022-09-23 18:13:11 -0300

    terceiro commit do matheus

--- Teste.java
+++ Teste.java
@@ -1,6 +1,5 @@
 public class Teste {
     public static void main(String[] args) {
-        System.out.println("Eu fiz uma modificação!");
-        System.out.println("Olha mãe, eu fiz uma modificação");
+        System.out.println("Testando os commits");
     }
 }
